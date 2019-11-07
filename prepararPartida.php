f85c0c72c7662e48c5a82f3195c247febba0a456
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2019-11-07 16:48:22 -0300

    Actualización de código

--- prepararPartida.php
+++ prepararPartida.php
@@ -1,5 +1,9 @@
 <?php
     $datos_barcos = $_POST["datos_barcos"];
     $datos_barcos_barcos = explode("_",$datos_barcos);
-    print_r($datos_barcos_barcos);
+    //print_r($datos_barcos_barcos);
+    foreach($datos_barcos_barcos as $barco){
+        $barco_datos = explode(":",$barco);
+        print_r($barco);
+    }
 ?>
