f570498e77c2db7294db886575fbbd0404c17679
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2019-11-12 12:51:43 -0300

    Actualización y nuevo código

---
+++ datosApp.php
@@ -0,0 +1,4 @@
+<?php
+    $nombre_app = "Batalla Naval";
+    $version_app = "0.1";
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include_once "datosApp.php";
+?>
+
+<html>
+    <head>
+        <meta http-equiv='Content-type' content='text/html;charset=UTF-8'>
+		<link rel="stylesheet" href="inicio.css">
+        <link rel="stylesheet" href="jquery-ui/jquery-ui.css">
+        <title>
+            <?php 
+                echo "$nombre_app - v $version_app";
+            ?>
+        </title>
+        <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Acme&display=swap" rel="stylesheet">
+        
+		<script type='text/javascript' src='jquery.js'></script>
+		<script type='text/javascript' src='posicionarBarcos_V2.js'></script>
+        <script type="text/javascript" src="jquery-ui/jquery-ui.js"></script>
+    </head>
+    <body>
+        
+        <div id="caja_centro" ></div>
+        
+    </body>
+</html>
